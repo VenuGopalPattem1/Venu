64b76dac1bb784189dd4e228761d2d56b8c16c0f
venuv <venuv@Venugopal>
2024-07-12 12:09:36 +0530

    stacj

---
+++ DemoProj/src/com/nt/stach/StashWork.java
@@ -0,0 +1,21 @@
+package com.nt.stach;
+
+public class StashWork {
+	public void meth1(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void meth2(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void meth3(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public void meth4(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	
+}
